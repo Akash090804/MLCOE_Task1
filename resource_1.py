76c9e6a7db3ab79901240e5cea82058964ffb2de
Akash Varshney <Akashvarshney65025@gmail.com>
2024-10-07 10:31:37 +0530

    resouce class defined

---
+++ resource_1.py
@@ -0,0 +1,7 @@
+class resource:
+  def initialisation(user,water=100,coffeeBean=50,milk=200,cups=20):
+    user.water=water
+    user.coffeeBean=coffeeBean
+    user.milk=milk
+    user.cups=cups
+    
