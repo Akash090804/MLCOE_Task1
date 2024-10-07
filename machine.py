2fb6f9c6daf916a416d374ed0da1f8049a71ed71
Akash Varshney <Akashvarshney65025@gmail.com>
2024-10-07 11:55:58 +0530

    new update

--- machine.py
+++ machine.py
@@ -8,4 +8,8 @@ class machine:
         used_coffee = int(input("Enter the amount of coffee beans (g): "))
         milk_used = int(input("Enter the amount of milk (ml): "))
         cup = int(input("Enter the number of cups: "))
+        if user.resources.using(used_water,used_coffee,milk_used,cup):
+            return f"coffee {type} is ready!!"
+        else:
+            return "You dont have enough resources for this {type}"
         
